261d38a952c295443fc9a7149f3f229c02950122
Vincent Hou <vincenthou@augmentum.com.cn>
2014-03-25 11:23:02 +0800

    Summary: Add nigix start and stop bat

---
+++ web-conf/nigix/start.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Windows 下无效
+REM set PHP_FCGI_CHILDREN=5
+
+REM 每个进程处理的最大请求数，或设置为 Windows 环境变量
+set PHP_FCGI_MAX_REQUESTS=1000
+ 
+echo Starting PHP FastCGI...
+RunHiddenConsole php-cgi.exe -b 127.0.0.1:9000 -c D:/wamp/bin/apache/apache2.2.22/bin/php.ini
+
+echo Statring mysql
+RunHiddenConsole net start wampmysqld
+
+echo Starting nginx...
+RunHiddenConsole c:/nginx/nginx.exe
